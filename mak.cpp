0a639a38db82b6990260975ac37d1c01857c1384
TAUPIK_TIF_RM_16C <ovikbinasehat@gmail.com>
2017-04-05 23:08:17 +0700

    Add files via upload

---
+++ mak.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int a,b;
+
+cout<<"==Menentukan Nilai Terbesar Dari Dua Inputan=="<<endl;
+cout<<"====================TAUPIK===================="<<endl;
+cout<<"Masukan Nilai A : "; cin>>a;
+cout<<"Masukan Nilai B : "; cin>>b;
+
+if (a>b)
+cout <<"Nilai Maksimum: "<<a<<endl;
+else 
+cout <<"Nilai Minimum : "<<b<<endl;
+
+return 0;
+}
